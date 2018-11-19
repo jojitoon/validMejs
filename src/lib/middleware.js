d90dba53323f4a3b0a28ede722da56582f3a4904
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2018-11-19 22:41:52 +0100

    commit

--- src/lib/middleware.js
+++ src/lib/middleware.js
@@ -2,62 +2,63 @@ import isEmail from 'validator/lib/isEmail';
 
 const options = validate => ({
   isEmail: (name='email', variable=name) => {
-    if (validate.req.body[name] && !isEmail(validate.req.body[name])) {
-      validate.errors.push(`${variable.charAt(0).toUpperCase() + variable.slice(1)} must be a valid email`);
-    }
-    
-    console.log(`${variable.charAt(0).toUpperCase() + variable.slice(1)} must be a valid email`)
+    checkAndPush(
+      validate.req.body[name] && !isEmail(validate.req.body[name]),
+      `${variable.charAt(0).toUpperCase() + variable.slice(1)} must be a valid email`,
+      validate );
     return options(validate);
   },
 
   hasElement: (item, variable=item) => {
-    if (!validate.req.body[item]) {
-      validate.errors.push(`${variable} is required`);
-    }
-    return options(validate);
+    checkAndPush(
+      !validate.req.body[item],
+      `${variable} is required`,
+      validate
+      );
+      return options(validate);
   },
 
   isLength: (item, value, variable=item) => {
-    if (validate.req.body[item] && validate.req.body[item].replace(/\s/g, '').length < value) {
-      validate.errors.push(`${variable} must have at least a length of ${value}`);
-    }
+    checkAndPush(
+      validate.req.body[item] && validate.req.body[item].replace(/\s/g, '').length < value,
+      `${variable} must have at least a length of ${value}`,
+      validate
+    );
     return options(validate);
   },
 
   hasSpaces: (item, variable=item) => {
-    if (validate.req.body[item] && /\s/.test(validate.req.body[item])) {
-      validate.errors.push(`${variable} must not have spaces`);
-    }
+    checkAndPush(
+      validate.req.body[item] && /\s/.test(validate.req.body[item]),
+      `${variable} must not have spaces`,
+      validate
+    );
     return options(validate);
   },
 
   isBool: (item, variable=item) => {
-    if (typeof validate.req.body[item] !== 'boolean') {
-      validate.errors.push(`${variable} must be a boolean`);
-    }
-    return options(validate);
+    checkTypeOf(validate.req.body[item], 'boolean', `${variable} must be a boolean`, validate);
+    return options(validate)
   },
 
   isNumber: (item, variable=item) => {
-    if (typeof validate.req.body[item] !== 'number' || isNaN(validate.req.body[item])) {
-      validate.errors.push(`${variable} must be a number`);
-    }
+    checkAndPush(
+      typeof validate.req.body[item] !== 'number' || isNaN(validate.req.body[item]),
+      `${variable} must be a number`,
+      validate
+    );
     return options(validate);
   },
 
   isString: (item, variable=item) => {
-    if (typeof validate.req.body[item] !== 'string') {
-      validate.errors.push(`${variable} must be a string`);
-    }
-    return options(validate);
+    checkTypeOf(validate.req.body[item], 'string', `${variable} must be a string`, validate);
+    return options(validate)
   },
 
   isDate: (item, variable=item) => {
       const date = new Date(validate.req.body[item]);
-    if (date.toDateString() === 'Invalid Date') {
-      validate.errors.push(`${variable} must be a date`);
-    }
-    return options(validate);
+      checkAndPush(date.toDateString() === 'Invalid Date', `${variable} must be a date`, validate);
+      return options(validate);
   },
 
 
@@ -81,21 +82,7 @@ const options = validate => ({
   },
 
   isDateFuture: (date, variable=date, compare = new Date()) => {
-    const checkDate = new Date(validate.req.body[date]);
-    const today = new Date(compare);
-    const message = `${variable} is an invalid date. Must not be further than ${today.toDateString()}`
-    checkDate.setUTCHours(0);
-    checkDate.setUTCMinutes(0);
-    checkDate.setUTCSeconds(0);
-    checkDate.setUTCMilliseconds(0);
-
-    today.setUTCHours(0);
-    today.setUTCMinutes(0);
-    today.setUTCSeconds(0);
-    today.setUTCMilliseconds(0);
-  if (checkDate > today) {
-    validate.errors.push(message);
-  }
+    checkPast(date, compare, variable, validate, false);
   return options(validate);
 },
 
@@ -115,3 +102,42 @@ const validate = (req, res, next) => options({
 });
 
 export default validate;
+
+const checkTypeOf = (item, type, message, validate) => {
+  if (typeof item !== type) {
+    validate.errors.push(message);
+  }
+};
+
+const checkAndPush = (condition, message, validate) => {
+  if(condition) {
+    validate.errors.push(message);
+  }
+};
+
+const checkPast = (date, compare, variable, validate, check) => {
+    const checkDate = new Date(validate.req.body[date]);
+    const today = new Date(compare);
+    const message = check
+                      ?`${variable} is an invalid date. Must not be later than ${today.toDateString()}`
+                      :`${variable} is an invalid date. Must not be further than ${today.toDateString()}`;
+
+    checkDate.setUTCHours(0);
+    checkDate.setUTCMinutes(0);
+    checkDate.setUTCSeconds(0);
+    checkDate.setUTCMilliseconds(0);
+
+    today.setUTCHours(0);
+    today.setUTCMinutes(0);
+    today.setUTCSeconds(0);
+    today.setUTCMilliseconds(0);
+    if (check) {
+      if (checkDate < today) {
+        validate.errors.push(message);
+      }
+    } else {
+      if (checkDate > today) {
+        validate.errors.push(message);
+      }
+    }
+};
